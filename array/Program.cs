22b7106375c9f3edef985c3dd960f192f8f732ee
rashadheydarli <rashadahe@code.edu.az>
2023-01-13 21:25:57 +0400

    array added

---
+++ array/Program.cs
@@ -0,0 +1,105 @@
+﻿//int number = 10;
+//int[] numbers = { 100, 200, 300 };
+//string[] students = { "Rashad", "Ruslan", };
+
+
+//int[] numbers1 = { 100, 200, 300, 400 };
+//for (int i = 0; i < numbers1.Length; i++)
+//{
+//    System.Console.WriteLine(numbers1[i]);
+//}
+
+//int[] n = { 1, 2, 3 };
+//for (int i = 0; i < n.Length; i++)
+//{
+//    System.Console.WriteLine(n[i]);
+//}
+
+//int[] n = { 1, 2, 3 };
+//int sum = 0;
+//for (int i = 0; i < n.Length; i++)
+//{
+//    sum += n[i];
+//}
+//    System.Console.WriteLine(sum);
+
+//int[] n = { 1, 2, 3};
+//int max = n[0]
+//for (int i=1; i<n.Length; i++)  
+//{
+//   if( max < n[i])
+//    max = n[i];
+//}
+//System.Console.WriteLine(max);
+
+//int[] n = { -1, -2, 1, 4 };
+//int sum = 0;
+//for (int i=0; i<n.Length; i++)
+//{
+//    sum += n[i];
+//}
+//System.Console.WriteLine(sum);
+
+//int[] n = { 6, 3, 5, 7, 12, 15 };
+//int min = n[0];
+//for (int i = 1; i < n.Length; i++)
+//{
+//    if (min > n[i])
+//    {
+//        min = n[i];
+//    }
+//}
+//System.Console.WriteLine(min);
+
+//int[] n = { 1, 2, 3, 4, 5 };
+//int count = 0;
+//for (int i = 0; i < n.Length; i++)
+//{
+//    count += n[i];
+//}
+//System.Console.WriteLine(count);
+
+//int[] n = { 100, 0, 30, 0 };
+//int count = 0;
+//for (int i = 0; i < n.Length; i++)
+//{
+//    if( n[i] == 0)
+//    {
+//        count++;
+//    }
+//}
+//System.Console.WriteLine(count);
+
+//int[] n = { -1, -2, 1, -4 };
+//int count = 0;
+//for (int i = 0; i < n.Length; i++)
+//{
+//    if(n[i]<0)
+//    {
+//        count++;
+//    }
+
+//}
+//System.Console.WriteLine(count);
+
+//int[] n = { -1, -2, 1, 4, -2 };
+//int sum = 0;
+//for (int i = 0; i < n.Length; i++)
+//{
+//    if(n[i]<0)
+//    {
+//        sum += n[i];
+//    }
+//}
+//System.Console.WriteLine(sum);
+
+//int[] n = { -1, -2, 1, 4 };
+//int count = 0;
+//for (int i = 0; i < n.Length; i++)
+//{
+//    if(n[i]>0)
+//    {
+//        count++;
+//    }
+//}
+//System.Console.WriteLine(count);
